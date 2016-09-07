ebbc31ed080ac3d6909b3e193afdb298e9c6a728
Bibek <bibek.thapahere@gmail.com>
2016-09-06 18:15:18 -0700

    Interface and Abstract class implementation

---
+++ src/ShootProject/Football.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ShootProject;
+
+/**
+ *
+ * @author HOME
+ */
+public class Football extends Shoot {
+
+    @Override
+    public void shoot() {
+        System.out.println("Football shoots");
+    }
+    
+    
+}
---
+++ src/ShootProject/Gun.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ShootProject;
+
+/**
+ *
+ * @author HOME
+ */
+public class Gun extends Shoot{
+
+    @Override
+    public void shoot() {
+        System.out.println("Gun shoots");
+    }
+    
+    
+    
+}
